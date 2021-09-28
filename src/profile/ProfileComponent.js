b728dcb46803b953b07a93c26965badf65292b6e
Rahma0911 <88459971+Rahma0911@users.noreply.github.com>
2021-09-28 23:58:36 +0100

    Add files via upload

--- src/profile/ProfileComponent.js
+++ src/profile/ProfileComponent.js
@@ -1,28 +1,34 @@
 import React from "react";
-import Photo from "./Photo";
-import picture from "./picture.jpg";
+import unknown from "./unknown.png";
 import PropTypes from "prop-types";
 
 const ProfileComponent = (props) => {
     const styleObject = {color:"blue" , textAlign:"left"}
+    function handleName () {
+        alert(props.fullName);
+    }
+
     return(
     <div style={styleObject}>
         <h1>{props.fullName}</h1>
         <p>{props.bio}</p>
         <h2>{props.profession}</h2>
-        <Photo src={picture} alt="" />
-        <handleName name = {props.fullName}/>;
+        <img src={props.imageURL} alt="" width="15%" />
+        <br></br>
+        <button onClick={handleName}>Show Name</button>
     </div>
 );
 };
 ProfileComponent.defaultProps = {
     fullName:"ANONYMOUS",
     bio:"BIO",
-    profession:"PROFESSION"  
+    profession:"PROFESSION" , 
+    imageURL: {unknown}
 };
 ProfileComponent.propTypes = {
     fullName:PropTypes.string,
     bio:PropTypes.string,
-    profession:PropTypes.string
+    profession:PropTypes.string,
+    imageURL:PropTypes.string
 };
 export default ProfileComponent;
