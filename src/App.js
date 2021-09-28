8eeaa17b42165b5e37370e43d0439794c8a67f5b
Rahma0911 <88459971+Rahma0911@users.noreply.github.com>
2021-09-28 23:56:47 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import picture from "./profile/picture.jpg";
 import ProfileComponent from "./profile/ProfileComponent";
 
 const App = () => {
+  return (
   <div>
-    <ProfileComponent fullName="Rahma Ismail" bio="" profession="" />
+    <ProfileComponent imageURL={picture} fullName="Rahma Ismail" bio="I’m a web developer. I spend my whole day, practically every day, 
+    experimenting with HTML, CSS, and JavaScript. I build websites that delight and inform. I do it well. I’m 
+    curious, and I enjoy work that challenges me to learn something new and stretch in a different direction." 
+    profession="Web Developer" />
   </div>
+  )
 };
 
 export default App;
